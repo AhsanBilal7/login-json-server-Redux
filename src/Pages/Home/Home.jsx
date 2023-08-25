856a4c640a6da4f08be81bad20aabcf786ec6534
AhsanBilal7 <abilal.bee20seecs@seecs.edu.pk>
2023-08-25 22:52:25 +0500

    Add Default Login Credentials in Home Page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { register_get } from '../Store/registerSlice';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 function Home() {
 const dispatch = useDispatch();
 
@@ -12,8 +13,24 @@ const dispatch = useDispatch();
 
   } ,[] )
 
-  return (
+  return (<>
+
     <div>Home</div>
+    <br/>
+    <br/>
+
+    <h1> {'{ Default Credentials }'}</h1>
+    <h3>Name:"Ahsan Bilal"</h3>
+    <h3>Password:"test"</h3>
+    <br/>
+    <br/>
+    <br/>
+    <h4>Please Follow this link to Check all Registered User</h4>
+    <Link target='_blank' to="https://my-json-server.typicode.com/AhsanBilal7/login-json-server-Redux/user">This Link</Link>
+    <h4>This App is used to Practice REDUX with React Persist</h4>
+
+
+  </>
   )
 }
 
